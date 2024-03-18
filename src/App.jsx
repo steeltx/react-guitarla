88c8a383aae97a571ef72c04c5992360accc0109
steeltx <realsteel.gnu@gmail.com>
2024-03-18 15:47:35 -0600

    Se crean los componentes de header y guitar, se iteran y  muestran

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
+
 function App() {
+
+	const [data, setData] = useState(db);
 
 	return (
 		<>
-			<h1>GuitarLA</h1>
+			<Header />
+			<main className="container-xl mt-5">
+				<h2 className="text-center">Nuestra Colección</h2>
+
+				<div className="row mt-5">
+					{
+						data.map((guitar) => (
+							<Guitar 
+								key={guitar.id}
+								guitar={guitar}	
+							/>
+						))
+					}
+				</div>
+			</main>
+
+			<footer className="bg-dark mt-5 py-5">
+				<div className="container-xl">
+					<p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+				</div>
+			</footer>
 		</>
 	)
 }
